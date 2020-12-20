40a86333ae63cef93b1c092c1668e43e37aea58c
alfonso <alfonso@zipcodes-MacBook-Pro.local>
2020-12-20 13:52:46 -0500

    initial commit

--- booksapi/books/models.py
+++ booksapi/books/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import csv
 # Create your models here.
 
 class Book(models.Model):
@@ -13,3 +13,19 @@ class Book(models.Model):
 
     def __str__(self):
         return self.name
+
+
+path = "/Users/alfonso/micropython/micropythonapi/bestsellers-with-categories.csv"
+with open(path) as f:
+    reader = csv.reader(f)
+    next(reader, None)  # skip the headers
+    for row in reader:
+        _, created = Book.objects.get_or_create(
+            name=row[0],
+            author=row[1],
+            rating=row[2],
+            reviews=row[3],
+            price=row[4],
+            year=row[5],
+            genre=row[6],
+        )

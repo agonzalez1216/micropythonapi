8238837bee29a7ce6c8129a833236f6ac77052ef
alfonso <alfonso@zipcodes-MacBook-Pro.local>
2020-12-20 16:19:48 -0500

    DB change

--- booksapi/booksapi/settings.py
+++ booksapi/booksapi/settings.py
@@ -19,11 +19,12 @@ print(BASE_DIR)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+print(STATIC_ROOT)
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'staticfiles/booksapi'),
 )
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
